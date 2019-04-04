0b526ec4fb03d51c86530e09b9333426cc906471
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2019-04-04 13:15:28 +0300

    Version 1.0.7

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-const toggleMenu = function() {
-  const toggle = document.getElementById("toggle");
-  const body = document.getElementsByTagName("BODY")[0];
+"use strict";
+
+var toggleMenu = function toggleMenu() {
+  var toggle = document.getElementById("toggle");
+  var body = document.getElementsByTagName("BODY")[0];
   document.getElementById("nav").classList.toggle("show");
   toggle.classList.toggle("on");
   body.classList.toggle("menu-open");
 };
+
 toggle.addEventListener("click", toggleMenu);
 
-window.onscroll = () => {
-  const nav = document.getElementById("nav-big");
+window.onscroll = function() {
+  var nav = document.getElementById("nav-big");
   if (this.scrollY > 10) nav.classList.add("scroll");
   if (this.scrollY <= 10) nav.classList.remove("scroll");
 };
+
+// const toggleMenu = function() {
+//   const toggle = document.getElementById("toggle");
+//   const body = document.getElementsByTagName("BODY")[0];
+//   document.getElementById("nav").classList.toggle("show");
+//   toggle.classList.toggle("on");
+//   body.classList.toggle("menu-open");
+// };
+// toggle.addEventListener("click", toggleMenu);
+
+// window.onscroll = () => {
+//   const nav = document.getElementById("nav-big");
+//   if (this.scrollY > 10) nav.classList.add("scroll");
+//   if (this.scrollY <= 10) nav.classList.remove("scroll");
+// };
